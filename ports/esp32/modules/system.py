3894f20a5f7d6bce438cd6940d1ff1d2474ffbdb
Big Circle <794557226@qq.com>
2019-07-03 18:20:44 +0800

    在system.py中添加一个返回_module_info列表的函数。

--- ports/esp32/modules/system.py
+++ ports/esp32/modules/system.py
@@ -24,32 +24,29 @@ _module_key_list = list(DEVICE_TYPE.keys())
 _module_value_list = list(DEVICE_TYPE.values())
 _m_value_position = 1
 _module_info = []
-# print(1111, time.ticks_ms())
-mf = open('sysCfg.py', 'wb')
-# print(2222, time.ticks_ms())
-for i in range(_m_info[0]):
-    if _m_info[_m_value_position] in _module_value_list:
-        mStr = _module_key_list[_module_value_list.index(
-            _m_info[_m_value_position])]
-        mStr2 = mStr[0].upper() + mStr[1:]
-        _m_value_position = _m_value_position + 1
-        mf.write(bytearray('from ' + mStr2 + ' import ' + mStr2 + '\r\n'))
-        # print('from ' + mStr2 + ' import ' + mStr2)
-        for j in range(_m_info[_m_value_position]):
-            mf.write(
-                bytearray(mStr + str(j + 1) + '=' + mStr2 + '(' + str(j + 1) +
-                          ')\r\n'))
-            # print(mStr + str(j + 1) + '=' + mStr2 + '(' + str(j + 1) + ')')
-            _module_info.append(mStr + str(j + 1))
-        _m_value_position = _m_value_position + 1
-        #         # print(mStr)
-        del mStr
-        del j
-    else:
-        _m_value_position = _m_value_position + 2
-        print('not support module!')
+with open('sysCfg.py', 'wb') as mf:
+    for i in range(_m_info[0]):
+        if _m_info[_m_value_position] in _module_value_list:
+            mStr = _module_key_list[_module_value_list.index(
+                _m_info[_m_value_position])]
+            mStr2 = mStr[0].upper() + mStr[1:]
+            _m_value_position = _m_value_position + 1
+            mf.write(bytearray('from ' + mStr2 + ' import ' + mStr2 + '\r\n'))
+            # print('from ' + mStr2 + ' import ' + mStr2)
+            for j in range(_m_info[_m_value_position]):
+                mf.write(
+                    bytearray(mStr + str(j + 1) + '=' + mStr2 + '(' + str(j + 1) +
+                            ')\r\n'))
+                # print(mStr + str(j + 1) + '=' + mStr2 + '(' + str(j + 1) + ')')
+                _module_info.append(mStr + str(j + 1))
+            _m_value_position = _m_value_position + 1
+            #         # print(mStr)
+            del mStr
+            del j
+        else:
+            _m_value_position = _m_value_position + 2
+            print('not support module!')
 
-mf.close()
 del mf
 del _m_info
 del _module_key_list
@@ -73,5 +70,5 @@ del framehub
 del time
 del gc
 
-# from Display import Display
-# a = Display()
+def getModuleInformation():
+    return _module_info

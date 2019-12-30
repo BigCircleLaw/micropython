6c9a1d76f73f3e63f28ef05c7a58868d31534a7f
Big Circle <794557226@qq.com>
2019-12-30 17:08:35 +0800

    修改版本号格式； 修改wonder为Pi。

--- ports/esp32/modules/wonderbits.py
+++ ports/esp32/modules/wonderbits.py
@@ -5,7 +5,7 @@ from public import DEVICE_TYPE
 import os
 
 VERSION = (0, 3, 0)
-__version__ = '.'.join(map(str, VERSION))
+__version__ = 'mPython'+'.'.join(map(str, VERSION))
 
 state = 'main.py' not in os.listdir()
 
@@ -66,4 +66,4 @@ class wb_tool(object):
 
 
 from Event import Event
-from Wonder import wonder
+from Pi import Pi

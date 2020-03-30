282730f939519975bd0a074d3b2b9e42f400a25b
Big Circle <794557226@qq.com>
2020-03-30 17:45:59 +0800

    修改makefile，可以选的打包固件的的py文件； 添加manfest_wb文件，用于选择包涵的py文件； 删除放在micropython库中的wbmp-sysy文件。

---
+++ ports/esp32/manfest_wb.py
@@ -0,0 +1,6 @@
+include("boards/manifest.py")
+
+# freeze("$(PORT_DIR)/wb_modules")
+freeze("../../../WonderBits-Python/wbmp-sys")
+freeze("../../../WonderBits-Python/wbmp-wonderbits")
+freeze("../../../WonderBits-Python/wbmp-modules")

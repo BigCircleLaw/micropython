fcc8292f1fc236960bec2f5516a62ac68f8e297a
Big Circle <794557226@qq.com>
2020-03-31 17:54:48 +0800

    完成新版文件结构在mPython上的适配。

--- ports/esp32/manfest_wb.py
+++ ports/esp32/manfest_wb.py
@@ -2,5 +2,5 @@ include("boards/manifest.py")
 
 freeze("$(PORT_DIR)/wb_modules")
 freeze("../../../WonderBits-Python/wbmp-sys")
-freeze("../../../WonderBits-Python/wbmp-wonderbits")
+freeze("../../../WonderBits-Python/wbmp-mPython")
 freeze("../../../WonderBits-Python/wbmp-modules")
